c8b05f8ffc4dc6a1a48ea5f5405c035a045303c4
Aamir ullah <105126869+Aamir-Ullah@users.noreply.github.com>
2023-04-27 16:23:14 +0500

    change in users

--- users/dashboard.php
+++ users/dashboard.php
@@ -9,102 +9,37 @@ include('config/dbconn.php');
 include('includes/header.php');
 include('includes/sidebar.php');
 ?>
-<!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0">Dashboard</h1>
-                </div><!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
-                        <li class="breadcrumb-item"><a href="logout.php">Logout</a></li>
-                        <li class="breadcrumb-item active"><?php echo $user; ?></li>
-                    </ol>
-                </div><!-- /.col -->
-            </div><!-- /.row -->
-<<<<<<< HEAD
-        </div><!-- /.container-fluid -->
-</div>
-<!-- Main content -->
-<section class="content">
-    <div class="container-fluid">
-        <div class="row">
-            <?php
-                 $sql1 = "SELECT *FROM user_car JOIN park_slot WHERE user_car.parking_slots=park_slot.slot_id;";
-                 $res = mysqli_query($conn, $sql1);
-                 while ($row = mysqli_fetch_assoc($res)) {
-                 ?>
-            <?php
-                 if ($row['slot_status'] == '1') {
-                 ?>
-            <div class="col-2 p-4 pb-4 bg-danger text-white m-2 text-center">
-                <?php  echo $row['id']."<br>"; ?>
-                <?php  echo $row['slot_name']."<br>"; ?>
-                <?php  echo $row['parking_address']."<br>"; ?>
-                <?php  echo $row['parking_area']."<br>"; ?>
+<div class="content-wrapper" style="background-color:rgb(254, 254, 255)">
+    <div class="container">
+        <div class="row mr-4">
+            <div class="btn btn-success mt-4 ml-4 col-3">
+                Available
             </div>
-
-            <?php
-                } else {
-                 ?>
-            <div class="col-2 p-4 pb-4 bg-success text-white m-2 text-center">
-
+            <div class="btn btn-danger mt-4 ml-4 col-3">
+                Unavailable
             </div>
-
-            <?php
-            }
-            ?>
-
-            <?php
-        }
-
-        ?>
-</section>
-=======
-            <section class="content">
-                <div class="container-fluid">
-                    <div class="row">
-                        <?php
-                        $sql1 = "SELECT * FROM user_car JOIN park_slot WHERE user_car.parking_slots=park_slot.slot_id";
-                        // $sql1 = "SELECT * FROM park_slot WHERE 1";
-                        $res = mysqli_query($conn, $sql1);
-                        while ($row = mysqli_fetch_assoc($res)) {
-                        ?>
-                            <?php
-                            if ($row['availability_status'] == 'unavailable') {
-                            ?>
-                                <div class="col-2 p-4 bg-danger text-white m-2">
-                                    <ul class="text-start">
-                                        <li><?php echo $row['id'] . "<br>"; ?></li>
-                                        <li><?php echo $row['slot_name'] . "<br>"; ?></li>
-                                        <li><?php echo $row['parking_address'] . "<br>"; ?></li>
-                                        <li><?php echo $row['parking_area'] . "<br>"; ?></li>
-                                    </ul>
-                                </div>
-                            <?php
-                            } else {
-                            ?>
-                                <div class="col-2 p-4 bg-success text-white m-2">
-                                    <ul class="text-start">
-                                        <li><?php echo $row['slot_name'] . "<br>"; ?></li>
-                                    </ul>
-                                </div>
-                            <?php
-                            }
-                            ?>
-                        <?php
-                        }
-                        ?>
-                    </div>
-                </div>
-            </section>
-        </div><!-- /.container-fluid -->
+        </div>
+        <div class="row mt-4 ml-4">
+            <div style="display:table;height:200px" class="col-3 ml-1">
+                <a style="display:table-cell;background-color:green;text-decoration: none;padding:50px;text-align:center;"
+                    href="dashboard.php"></a>
+            </div>
+            <div style="display:table;" class="col-3 ml-1">
+                <a style="display:table-cell;background-color:green;text-decoration: none;padding:50px;text-align:center;"
+                    href="dashboard.php"></a>
+            </div>
+            <div style="display:table;" class="col-3 ml-1">
+                <a style="display:table-cell;background-color:green;text-decoration: none;padding:50px;text-align:center;"
+                    href="dashboard.php"></a>
+            </div>
+            <div style="display:table;height:200px" class="col-3 ml-1 mt-4">
+                <a style="display:table-cell;background-color:green;text-decoration: none;padding:50px;text-align:center;"
+                    href="dashboard.php"></a>
+            </div>
+        </div>
     </div>
->>>>>>> d8dcb2597998c835925698acbee85267b9d8efeb
+</div>
+</div>
 </div>
 <?php
 include('includes/footer.php');
