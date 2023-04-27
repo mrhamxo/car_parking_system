d3f2e1b9659762d35243a9977e1d41e5bf35c780
Muhammad Hamza <mr.hamxa942@gmail.com>
2023-04-27 10:52:39 +0500

    availability_status updated in admin and users

--- users/dashboard.php
+++ users/dashboard.php
@@ -10,10 +10,6 @@ include('config/dbconn.php');
 include('includes/header.php');
 include('includes/topbar.php');
 include('includes/sidebar.php');
-<<<<<<< HEAD
-=======
-
->>>>>>> d8dcb2597998c835925698acbee85267b9d8efeb
 ?>
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
@@ -32,85 +28,46 @@ include('includes/sidebar.php');
                     </ol>
                 </div><!-- /.col -->
             </div><!-- /.row -->
-<<<<<<< HEAD
-        </div><!-- /.container-fluid -->
-</div>
-<!-- Main content -->
-<section class="content">
-    <div class="container-fluid">
-        <div class="row">
-            <?php
-                 $sql1 = "SELECT *FROM user_car JOIN park_slot WHERE user_car.parking_slots=park_slot.slot_id;";
-                 $res = mysqli_query($conn, $sql1);
-                 while ($row = mysqli_fetch_assoc($res)) {
-                 ?>
-            <?php
-                 if ($row['slot_status'] == '1') {
-                 ?>
-            <div class="col-2 p-4 pb-4 bg-danger text-white m-2 text-center">
-                <?php  echo $row['id']."<br>"; ?>
-                <?php  echo $row['slot_name']."<br>"; ?>
-                <?php  echo $row['parking_address']."<br>"; ?>
-                <?php  echo $row['parking_area']."<br>"; ?>
-            </div>
 
-            <?php
-                } else {
-                 ?>
-            <div class="col-2 p-4 pb-4 bg-success text-white m-2 text-center">
-
-            </div>
-
-            <?php
-            }
-            ?>
-
-            <?php
-        }
-
-        ?>
-</section>
-=======
-            <section class="content">
-                <div class="container-fluid">
-                    <div class="row">
-                        <?php
-                        $sql1 = "SELECT * FROM user_car JOIN park_slot WHERE user_car.parking_slots=park_slot.slot_id";
-                        // $sql1 = "SELECT * FROM park_slot WHERE 1";
-                        $res = mysqli_query($conn, $sql1);
-                        while ($row = mysqli_fetch_assoc($res)) {
-                        ?>
-                            <?php
-                            if ($row['availability_status'] == 'unavailable') {
-                            ?>
-                                <div class="col-2 p-4 bg-danger text-white m-2">
-                                    <ul class="text-start">
-                                        <li><?php echo $row['id'] . "<br>"; ?></li>
-                                        <li><?php echo $row['slot_name'] . "<br>"; ?></li>
-                                        <li><?php echo $row['parking_address'] . "<br>"; ?></li>
-                                        <li><?php echo $row['parking_area'] . "<br>"; ?></li>
-                                    </ul>
-                                </div>
-                            <?php
-                            } else {
-                            ?>
-                                <div class="col-2 p-4 bg-success text-white m-2">
-                                    <ul class="text-start">
-                                        <li><?php echo $row['slot_name'] . "<br>"; ?></li>
-                                    </ul>
-                                </div>
-                            <?php
-                            }
-                            ?>
-                        <?php
-                        }
-                        ?>
-                    </div>
-                </div>
-            </section>
         </div><!-- /.container-fluid -->
     </div>
->>>>>>> d8dcb2597998c835925698acbee85267b9d8efeb
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <?php
+                $sql1 = "SELECT *FROM user_car JOIN park_slot WHERE user_car.parking_slots=park_slot.slot_id;";
+                $res = mysqli_query($conn, $sql1);
+                while ($row = mysqli_fetch_assoc($res)) {
+                ?>
+                    <?php
+                    if ($row['availability_status'] == '0') {
+                    ?>
+                        <div class="col-2 p-4 pb-4 bg-danger text-white m-2 text-center">
+                            <?php echo $row['id'] . "<br>"; ?>
+                            <?php echo $row['slot_name'] . "<br>"; ?>
+                            <?php echo $row['parking_address'] . "<br>"; ?>
+                            <?php echo $row['parking_area'] . "<br>"; ?>
+                        </div>
+
+                    <?php
+                    } else {
+                    ?>
+                        <div class="col-2 p-4 pb-4 bg-success text-white m-2 text-center">
+                            <?php echo $row['slot_name'] . "<br>"; ?>
+                        </div>
+
+                    <?php
+                    }
+                    ?>
+
+                <?php
+                }
+
+                ?>
+    </section>
+</div><!-- /.container-fluid -->
+</div>
 </div>
 <?php
 include('includes/footer.php');
