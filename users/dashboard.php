1881002fd67823ef8a184c1d5fb48fb80543569d
Aamir ullah <105126869+Aamir-Ullah@users.noreply.github.com>
2023-04-28 11:42:31 +0500

    change in user

--- users/dashboard.php
+++ users/dashboard.php
@@ -22,19 +22,19 @@ include('includes/sidebar.php');
         <div class="row mt-4 ml-4">
             <div style="display:table;height:200px" class="col-3 ml-1">
                 <a style="display:table-cell;background-color:green;text-decoration: none;padding:50px;text-align:center;"
-                    href="dashboard.php"></a>
+                    href="uet_slot.php"></a>
             </div>
             <div style="display:table;" class="col-3 ml-1">
                 <a style="display:table-cell;background-color:green;text-decoration: none;padding:50px;text-align:center;"
-                    href="dashboard.php"></a>
+                    href="uet1_slot.php"></a>
             </div>
             <div style="display:table;" class="col-3 ml-1">
                 <a style="display:table-cell;background-color:green;text-decoration: none;padding:50px;text-align:center;"
-                    href="dashboard.php"></a>
+                    href="uet2_slot.php"></a>
             </div>
             <div style="display:table;height:200px" class="col-3 ml-1 mt-4">
                 <a style="display:table-cell;background-color:green;text-decoration: none;padding:50px;text-align:center;"
-                    href="dashboard.php"></a>
+                    href="uet3_slot.php"></a>
             </div>
         </div>
     </div>
